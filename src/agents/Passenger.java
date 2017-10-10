cc4c1a29e2792ea513721491acb306ce7bf4f2b3
PedroPachecoInf <pedrodanielpacheco96@hotmail.com>
2017-10-10 15:49:27 +0100

    Passenger creation

--- src/agents/Passenger.java
+++ src/agents/Passenger.java
@@ -1,5 +1,44 @@
 package agents;
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
 
-public class Passenger {
 
+public class Passenger extends Agent{
+	
+	private boolean inTaxi;
+	int xi,yi,xf,yf;
+	
+	public Passenger(int xi,int yi,int xf,int yf){
+		this.xi=xi;
+		this.yi=yi;
+		this.xf=xf;
+		this.yf=yf;
+		this.inTaxi=false;
+		
+	}
+	
+	
+	
+	public boolean getInTaxi (){
+		 
+		return this.inTaxi;
+	}
+	
+	public void setInTaxi(boolean inTaxi){
+		
+		this.inTaxi=inTaxi;
+	}
+	
+	
+	
+	
 }
+
+
+
+
